df3d84cfb7c87c66554a2bf3cb69c20fbd885225
unknown <qmutawali@gmail.com>
2024-09-29 17:29:40 +0700

    Konflik 2

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,12 @@ const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: isProd ? '/nama-repo-kamu' : '',
-  assetPrefix: isProd ? '/nama-repo-kamu/' : '',
+  output: 'export',
+  basePath: isProd ? '/web-services-app' : '',
+  assetPrefix: isProd ? '/web-services-app/' : '',
+  images: {
+    unoptimized: true, // Ini bisa membantu jika Anda mengalami masalah dengan gambar
+  },
 };
 
 export default nextConfig;
