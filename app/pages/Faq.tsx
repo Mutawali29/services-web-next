6d981b8556ecd5e19f10fc13efa54ef8c63fadea
Mutawali29 <164450088+Mutawali29@users.noreply.github.com>
2024-09-30 20:37:22 +0700

    Update Faq.tsx

--- app/pages/Faq.tsx
+++ app/pages/Faq.tsx
@@ -66,7 +66,7 @@ export function Faq() {
                   </p>
                   <span>
                     <Image
-                      src="./assets/plus.svg"
+                      src="./assets/feature-1.svg"
                       alt="See more"
                       width={24}
                       height={24}
